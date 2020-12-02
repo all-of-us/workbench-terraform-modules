fdc23fa62de2d08cbc3040d57870e40a6b4f80c4
Jay Carlton <53479492+jaycarlton@users.noreply.github.com>
2020-12-02 14:55:25 -0500

    try templating the query text separately

--- modules/workbench/modules/egress_detection/main.tf
+++ modules/workbench/modules/egress_detection/main.tf
@@ -4,9 +4,24 @@ locals {
   content_dir           = pathexpand("${path.module}/assets/content")
   search_configs        = fileset(local.content_dir, "*.json")
   content_template_path = pathexpand("${local.content_dir}/egress_window_template.json")
+  query_path            = pathexpand("${local.content_dir}/query.txt")
 }
 
-# Simply export a content file or folder and put the JSON file in ./assets/content
+# Nested calls to templatefile() aren't possible, so maybe doing a template data block
+# for the inner query file will work. https://registry.terraform.io/providers/hashicorp/template/latest/docs/data-sources/file
+data "template_file" "query_text" {
+  for_each = var.sumologic_egress_thresholds
+  template = file(local.query_path)
+  vars = {
+    aou_env              = var.aou_env
+    egress_threshold_mib = lookup(each.value, "egress_threshold_mib", 0)
+    egress_window_sec    = lookup(each.value, "egress_window_sec", 0)
+  }
+}
+
+# Simply export a content file or folder and put the JSON file in ./assets/content.
+# Since the query is so long (and critical) and is json-encoded, it's easier
+# to configure it separately.
 resource "sumologic_content" "main" {
   for_each  = var.sumologic_egress_thresholds
   parent_id = data.sumologic_personal_folder.aou_rw_egress_alerts.id
@@ -14,5 +29,7 @@ resource "sumologic_content" "main" {
     aou_env              = var.aou_env
     egress_threshold_mib = lookup(each.value, "egress_threshold_mib", 0)
     egress_window_sec    = lookup(each.value, "egress_window_sec", 0)
+
+    query_text = "${jsonencode(data.template_file.query_text[each.key].rendered)}"
   })
 }
