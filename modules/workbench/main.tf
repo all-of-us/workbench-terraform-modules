a2f30624a3eec8bc9d5fc2ffd5e78b79c3bdad89
yonghao <yyhao1@gmail.com>
2021-01-07 19:38:22 -0500

    fix

--- modules/workbench/main.tf
+++ modules/workbench/main.tf
@@ -11,7 +11,9 @@ module "reporting" {
 }
 
 module "egress_detection" {
-  source                      = "./modules/egress_detection"
-  sumologic_egress_thresholds = var.sumologic_egress_thresholds
-  aou_env                     = var.aou_env
+  source                                  = "./modules/egress_detection"
+  aou_env                                 = var.aou_env
+  sumologic_egress_thresholds             = var.sumologic_egress_thresholds
+  sumologic_parent_folder_id_hexadecimal  = var.aou_env
+  sumologic_webhook_id_hexadecimal        = var.aou_env
 }
