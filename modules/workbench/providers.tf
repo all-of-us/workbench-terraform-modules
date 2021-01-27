2a69b2a7282a25412fb01be20eb9d305af46803e
yonghao <yyhao1@gmail.com>
2021-01-26 23:45:09 -0500

    test egress

--- modules/workbench/providers.tf
+++ modules/workbench/providers.tf
@@ -2,27 +2,19 @@
 // Child modules receive their provider configurations from the root module.
 terraform {
   required_providers {
-    google = {
-      source = "hashicorp/google"
-    }
+    google = ">= 3.23.0"
     sumologic = {
-      source  = "SumoLogic/sumologic"
-      version = "2.6.0"
+      source = "terraform-providers/sumologic"
     }
   }
 }
 
-# To workaround import issue
-# `The configuration for module.workbench.provider["registry.terraform.io/hashicorp/google"] depends on
-# values that cannot be determined until apply.`
-# " https://github.com/hashicorp/terraform/issues/26211#issuecomment-705084351
-# Uncomment to replace provider block when performing terraform import.
-provider "google" {
-  version = "3.5.0"
-  project = "all-of-us-workbench-test"
-  region  = "us-central1"
-  zone    = "us-central1-c"
-}
+//provider "google" {
+//  version = "3.53.0"
+//  project = local.project_id
+//  region  = local.region
+//  zone    = local.zone
+//}
 //
 //provider "google" {
 //  version = "3.5.0"
