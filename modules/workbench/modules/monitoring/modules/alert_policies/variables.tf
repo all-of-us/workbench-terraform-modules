bf5fdad17be18c9bc1c068ee96ce3b156480b45b
CH Albach <calbach@google.com>
2021-08-06 16:25:58 -0700

    [RW-7086][risk=no] paramterize alerting thresholds

--- modules/workbench/modules/monitoring/modules/alert_policies/variables.tf
+++ modules/workbench/modules/monitoring/modules/alert_policies/variables.tf
@@ -22,3 +22,29 @@ variable expected_instance_count {
   description = "The expected number of instances. It is used for too_few_instances alert."
   type        = number
 }
+
+variable max_response_95p_latency_ms {
+  description = "Alert if we see our API 95 percentile latency exceed this"
+  type        = number
+}
+
+variable max_5xx_error_rate {
+  description = "Alert if we see our API returns 5XX errors above this rate for a duration"
+  type        = number
+}
+
+variable min_buffer_projects_registered {
+  description = "Alert if we have fewer than this many projects in the registered tier buffer"
+  type        = number
+}
+
+variable min_buffer_projects_controlled {
+  description = "Alert if we have fewer than this many projects in the controlled tier buffer"
+  type        = number
+}
+
+variable max_buffer_errors_per_minute {
+  description = "Alert if we see billing project buffer errors above this rate"
+  type        = number
+}
+
